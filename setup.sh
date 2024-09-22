651b764eeb94e43e89345227cb6cb984fe5f1697
han <744475118@qq.com>
2024-09-22 18:49:07 +0800

    ignore

--- setup.sh
+++ setup.sh
@@ -3,6 +3,4 @@ if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
    echo 'source ~/rda_ws/devel/setup.bash' >> ~/.bashrc
 elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
    echo 'source ~/rda_ws/devel/setup.zsh' >> ~/.zshrc
-else
-   # assume something else
 fi

0cba14d2511f27cebf258202387f7ff8b0e24f6d
han <744475118@qq.com>
2024-09-25 00:14:08 +0800

    format

--- script/circle_obstacle_from_gazebo.py
+++ script/circle_obstacle_from_gazebo.py
@@ -5,12 +5,12 @@ from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
 
 
 model_radius = 0.05
-model_name = 'agent'
+model_name = "agent"
 obstacles_msg = ObstacleArrayMsg()
 
 
 def obs_state_callback(data):
-    
+
     global model_name, model_radius, obstacles
 
     obstacles_msg.obstacles = []
@@ -25,36 +25,25 @@ def obs_state_callback(data):
 
             obstacles_msg.obstacles.append(obstacle)
 
-if __name__ == '__main__':
 
-    model_radius = rospy.get_param('model_radius', 0.05)
-    model_name = rospy.get_param('model_name', 'agent')
+if __name__ == "__main__":
 
-    rospy.init_node('gazebo_rda_convert_node', anonymous=True)
-    rospy.Subscriber('gazebo/model_states', ModelStates, obs_state_callback)
-    obs_pub = rospy.Publisher('rda_obstacles', ObstacleArrayMsg, queue_size=10)
+    model_radius = rospy.get_param("model_radius", 0.05)
+    model_name = rospy.get_param("model_name", "agent")
+
+    rospy.init_node("gazebo_rda_convert_node", anonymous=True)
+    rospy.Subscriber("gazebo/model_states", ModelStates, obs_state_callback)
+    obs_pub = rospy.Publisher("rda_obstacles", ObstacleArrayMsg, queue_size=10)
 
     rate = rospy.Rate(50)
 
     while not rospy.is_shutdown():
-        
+
         if len(obstacles_msg.obstacles) == 0:
-            rospy.logwarn_throttle(1, 'No obstacle detected')
+            rospy.logwarn_throttle(1, "No obstacle detected")
             continue
         else:
             obs_pub.publish(obstacles_msg)
-            rospy.loginfo_throttle(5, 'Publish rda obstacles successfully') 
+            rospy.loginfo_throttle(5, "Publish rda obstacles successfully")
 
         rate.sleep()
-
-    
-
-
-
-
-
-
-
-
-
-
